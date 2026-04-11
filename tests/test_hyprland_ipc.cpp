9bfd316aa3e2784a9a108e108c3cc0e13846c204
Bruno Rodrigues Onissanti <brunoonissanti@gmail.com>
2026-04-10 21:38:18 -0300

    fix(hyprlandIPC): fix setPath setting null value

--- tests/test_hyprland_ipc.cpp
+++ tests/test_hyprland_ipc.cpp
@@ -5,14 +5,14 @@
 
 Test(hyprland_ipc, set_path) {
     HyprlandIPC ipc;
-    std::string new_path = "/tmp/test_socket.sock";
+    std::string new_path = "/mock/test_socket.sock";
     ipc.SetPath(new_path);
     cr_assert_eq(ipc.GetPath(), new_path, "Path should be updated to %s, but got %s", new_path.c_str(), ipc.GetPath().c_str());
 }
 
 Test(hyprland_ipc, initialization) {
-    setenv("XDG_RUNTIME_DIR", "/tmp", 1);
+    setenv("XDG_RUNTIME_DIR", "/mock", 1);
     HyprlandIPC ipc;
-    std::string expected = "/tmp/hypr/HYPRLAND_INSTANCE_SIGNATURE/.socket.sock";
+    std::string expected = "/mock/hypr/HYPRLAND_INSTANCE_SIGNATURE/.socket.sock";
     cr_assert_eq(ipc.GetPath(), expected, "Initial path should be %s, but got %s", expected.c_str(), ipc.GetPath().c_str());
 }
